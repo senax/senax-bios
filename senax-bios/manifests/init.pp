2cd3d99561e55a080e747a50590f56744b9c8bb1
frank <frank@p1.thuis.moona.net>
2014-07-13 09:51:56 +0100

    Conversion to 3.6 module with metadata.json

--- senax-bios/manifests/init.pp
+++ senax-bios/manifests/init.pp
@@ -15,16 +15,16 @@
 # Here you should define a list of variables that this module would require.
 #
 # [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if it
-#   has a default. e.g. "The parameter enc_ntp_servers must be set by the
+#   Explanation of how this variable affects the funtion of this class and if
+#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
 #   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should not be used in preference to class parameters  as of
-#   Puppet 2.6.)
+#   global variables should be avoided in favor of class parameters as
+#   of Puppet 2.6.)
 #
 # === Examples
 #
 #  class { bios:
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ]
+#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
 #  }
 #
 # === Authors
@@ -33,7 +33,7 @@
 #
 # === Copyright
 #
-# Copyright 2013 Your name here, unless otherwise noted.
+# Copyright 2014 Your name here, unless otherwise noted.
 #
 class bios {
 
